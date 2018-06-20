474b501b099a8cf7689b802d175cdcdb1d4d3c95
Nick Schulz <grunendraache@gmail.com>
2018-06-19 21:43:37 -0400

    Section 2, Lecture 18

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.model.Author;
+import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author grune
+ */
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+    
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+    
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+    
+    private void initData() {
+        // Eric
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        eric.getBooks().add(ddd);
+        
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+        
+        // Rod
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", "Worx");
+        rod.getBooks().add(noEJB);
+        
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent e) {
+        initData();
+    }
+    
+}
